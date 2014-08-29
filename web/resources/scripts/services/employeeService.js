28dc795a93d0e03905ed9fe9cf1a85c843904250
FlorinSerban <serbanflorin92@gmail.com>
2014-08-29 10:36:27 +0300

    employee and project services updated

--- web/resources/scripts/services/employeeService.js
+++ web/resources/scripts/services/employeeService.js
@@ -3,7 +3,7 @@
 angular.module('qameleonApp.services.employee', [])
     .factory('Employee', ['$resource',
         function($resource) {
-            return $resource('http://192.168.1.29:8080/employees/addEmployee', {}, {
+            return $resource('http://localhost:8080/employees/addEmployee', {}, {
                 query: {
                     method: 'GET',
                     isArray: true
@@ -11,6 +11,22 @@ angular.module('qameleonApp.services.employee', [])
                 send: {
                     method: 'POST'
                 },
+                remove: {
+                    method: 'DELETE'
+                },
+                update: {
+                    method: 'PUT'
+                }
+            });
+        }
+    ])
+    .factory('EmployeeId', ['$resource',
+        function($resource) {
+            return $resource('http://localhost:8080/employees/:id', {}, {
+                query: {
+                    method: 'GET',
+                    isArray: true
+                },                
                 remove: {
                     method: 'DELETE'
                 },
